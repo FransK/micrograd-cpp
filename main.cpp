a08325cac3a18fb539e4208493ad1b810a120afb
FransK <kouwenhoven.frans@gmail.com>
2024-12-09 12:44:51 -0800

    aligning main with tutorial for verification

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include "value.cpp"
 
 int main() {
-    Value a = Value(1.0);
-    Value b = Value(3.0);
-    Value c = a + b;
-    std::cout << "Value of c: " << c << std::endl;
+    Value a = Value(2.0);
+    Value b = Value(-3.0);
+    Value c = Value(10.0);
+    Value d = a * b;
+    Value e = c + d;
+    Value f = Value(-2.0);
+    Value L = e * f;
+    std::cout << "Value of L: " << L << std::endl;
     return 0;
 }
