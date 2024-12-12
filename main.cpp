e97d8ad1ab5b503a7ae2ed422a9ac20e4ea05c7a
FransK <kouwenhoven.frans@gmail.com>
2024-12-11 18:57:07 -0800

    fixed accumulate function in main and made it more readable

--- main.cpp
+++ main.cpp
@@ -1,53 +1,48 @@
 #include <iostream>
 #include "module.h"
 
+using namespace std;
+
 int main() {
-    std::vector<int> nouts = {4, 4, 1};
-    auto n = std::make_shared<MLP>(3, nouts);
+    vector<int> nouts = {4, 4, 1};
+    auto n = make_shared<MLP>(3, nouts);
 
-    std::cout << n->to_string() << std::endl;
+    cout << n->to_string() << endl;
 
-    std::vector<std::vector<std::shared_ptr<Value>>> xs = {
-        {std::make_shared<Value>(2.0), std::make_shared<Value>(3.0), std::make_shared<Value>(-1.0)}, 
-        {std::make_shared<Value>(3.0), std::make_shared<Value>(-1.0), std::make_shared<Value>(0.5)}, 
-        {std::make_shared<Value>(0.5), std::make_shared<Value>(1.0), std::make_shared<Value>(1.0)},
-        {std::make_shared<Value>(1.0), std::make_shared<Value>(1.0), std::make_shared<Value>(-1.0)}
+    vector<vector<shared_ptr<Value>>> xs = {
+        {make_shared<Value>(2.0), make_shared<Value>(3.0), make_shared<Value>(-1.0)}, 
+        {make_shared<Value>(3.0), make_shared<Value>(-1.0), make_shared<Value>(0.5)}, 
+        {make_shared<Value>(0.5), make_shared<Value>(1.0), make_shared<Value>(1.0)},
+        {make_shared<Value>(1.0), make_shared<Value>(1.0), make_shared<Value>(-1.0)}
         };
-    std::vector<std::shared_ptr<Value>> ys = {std::make_shared<Value>(1.0), std::make_shared<Value>(-1.0), std::make_shared<Value>(-1.0), std::make_shared<Value>(1.0)};
+    vector<shared_ptr<Value>> ys = {make_shared<Value>(1.0), make_shared<Value>(-1.0), make_shared<Value>(-1.0), make_shared<Value>(1.0)};
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10000; i++) {
         // Forward pass
-        std::vector<std::vector<std::shared_ptr<Value>>> ypred;
+        vector<vector<shared_ptr<Value>>> ypred;
         for (auto& x : xs) {
             ypred.emplace_back((*n)(x));
         }
 
-        std::vector<std::shared_ptr<Value>> loss;
-        // Iterate using iterators
-        auto it_ys = ys.begin();
-        auto it_ypred = ypred.begin();
-
-        for (; it_ys != ys.end() && it_ypred != ypred.end(); ++it_ys, ++it_ypred) {
-            auto ygt = it_ys;  // Ground truth value
-            auto yout = it_ypred[0];  // Prediction value
-            loss.push_back((yout[0] - ygt[0])->pow(2.0));
-        }
-
-        auto total_loss = std::accumulate(loss.begin(), loss.end(), loss[0]);
+        // Add up the squared differences between y and ypred
+        vector<shared_ptr<Value>> ys = {make_shared<Value>(1.0), make_shared<Value>(-1.0), make_shared<Value>(-1.0), make_shared<Value>(1.0)};
+        auto loss = accumulate(ys.begin(), ys.end(), make_shared<Value>(0.0), [&](const auto &acc, const auto &yi) { 
+                auto diff = ypred[&yi - &ys[0]][0] - yi;
+                return acc + diff->pow(2.0); });
 
         // go backward
         n->zero_grad();
-        total_loss->buildGrads();
+        loss->buildGrads();
 
         // update
         for (auto& p : n->parameters()) {
-            p->data += -0.1 * p->grad;
+            p->data += -0.01 * p->grad;
         }
 
-        std::cout << i << ": " << total_loss->data << std::endl;
+        cout << i << ": " << loss->data << endl;
 
         for (auto& pred : ypred) {
-            std::cout << pred[0]->data << std::endl;
+            cout << pred[0]->data << endl;
         }
     }
 
